d0b62e6fb341ff343f0a46ab4dcc496eeb4bbf79
Samkeet3377 <samkeet3377@gmail.com>
2022-08-17 22:25:47 +0530

    home

--- JS/data/asset/js/script.js
+++ JS/data/asset/js/script.js
@@ -1,47 +1,47 @@
-const user = [
-    // {
-    //     'first_name': 'samkeet',
-    //     'last_name': 'kevat',
-    //     'contact': '123456',
-    //     'e_mail': 'samkeet@gmail.com',
-    //     'designation': 'front-end',
-    //     'from': 'bilimora',
-    // },
-    // {
-    //     'first_name': 'ravi',
-    //     'last_name': 'singh',
-    //     'contact': '123456',
-    //     'e_mail': 'ravi@gmail.com',
-    //     'designation': 'front-end',
-    //     'from': 'umargaon',
-    // },
-    // {
-    //     'first_name': 'nikunj',
-    //     'last_name': 'patel',
-    //     'contact': '123456',
-    //     'e_mail': 'nikunj@gmail.com',
-    //     'designation': 'front-end',
-    //     'from': 'chikhli',
-    // },
-    // {
-    //     'first_name': 'raj',
-    //     'last_name': 'patel',
-    //     'contact': '123456',
-    //     'e_mail': 'raj@gmail.com',
-    //     'designation': 'front-end',
-    //     'from': 'bilimora',
-    // },
-    // {
-    //     'first_name': 'kapil',
-    //     'last_name': 'singh',
-    //     'contact': '123456',
-    //     'e_mail': 'kapil@gmail.com',
-    //     'designation': 'front-end',
-    //     'from': 'valsad',
-    // }
-];
+// const user = [
+// {
+//     'first_name': 'samkeet',
+//     'last_name': 'kevat',
+//     'contact': '123456',
+//     'e_mail': 'samkeet@gmail.com',
+//     'designation': 'front-end',
+//     'from': 'bilimora',
+// },
+// {
+//     'first_name': 'ravi',
+//     'last_name': 'singh',
+//     'contact': '123456',
+//     'e_mail': 'ravi@gmail.com',
+//     'designation': 'front-end',
+//     'from': 'umargaon',
+// },
+// {
+//     'first_name': 'nikunj',
+//     'last_name': 'patel',
+//     'contact': '123456',
+//     'e_mail': 'nikunj@gmail.com',
+//     'designation': 'front-end',
+//     'from': 'chikhli',
+// },
+// {
+//     'first_name': 'raj',
+//     'last_name': 'patel',
+//     'contact': '123456',
+//     'e_mail': 'raj@gmail.com',
+//     'designation': 'front-end',
+//     'from': 'bilimora',
+// },
+// {
+//     'first_name': 'kapil',
+//     'last_name': 'singh',
+//     'contact': '123456',
+//     'e_mail': 'kapil@gmail.com',
+//     'designation': 'front-end',
+//     'from': 'valsad',
+// }
+// ];
 
-createtbl(user);
+// createtbl(user);
 
 function createtbl(data) {
     const tblbody = document.querySelector('.table-body'); // put table data
@@ -50,13 +50,15 @@ function createtbl(data) {
         const tblrow = document.createElement('tr'); // create row
         // console.log(emp);
         for (const key in emp) {
-            // if (['first_name', 'last_name', 'contact', 'e_mail', 'designation', 'from'].includes(key)) {
-                if (['id', 'name', 'username', 'email'].includes(key)) {
-                const datakey = emp[key];
-                const tblcol = document.createElement('td'); // create column
-                // console.log(tblcol);
-                tblcol.textContent = datakey; // put properties/index value in td
-                tblrow.appendChild(tblcol); // put table row value to column
+            if (Object.hasOwnProperty.call(emp, key)) {
+                // if (['first_name', 'last_name', 'contact', 'e_mail', 'designation', 'from'].includes(key)) {
+                if (['id','name', 'username', 'email', 'website'].includes(key)) {
+                    const datakey = emp[key];
+                    const tblcol = document.createElement('td'); // create column
+                    // console.log(tblcol);
+                    tblcol.textContent = datakey; // put properties/index value in td
+                    tblrow.appendChild(tblcol); // put table row value to column
+                }
             }
         }
         // put table column value to body
@@ -64,54 +66,41 @@ function createtbl(data) {
     });
 }
 
-let inputdata = document.getElementById('input');
-// const search = (e) => 
-function search (data){
-    var tcol, textvalue, i;
-    var find = false;
-    // var inputdata = document.getElementById('input');
-    var filter = inputdata.value.toUpperCase();
-    var tbl = document.getElementById('table');
-    var trow = tbl.getElementsByTagName('tr');
-    var msg = document.querySelector('#err-msg');
-    for (i = 0; i < trow.length; i++) {
-        // let dataValue = trow[i].getElementsByTagName("td")[0].value.toUpperCase();
+// 
+function searchdata(){
+    const tblbody = document.querySelector('.table-body');
+    const inputsearch = document.querySelector('#input');
+    tblbody.textContent = '';
+    let filter = [];
 
-        tcol = trow[i].getElementsByTagName('td')[0];
-        //var td = tcol.value.toUpperCase();
-        if (tcol) {
-            textvalue = tcol.textContent;
-            // || tcol.innerText;
-            if (textvalue.toUpperCase().indexOf(filter) > -1) {
-                trow[i].style.display = "";
-                find = true;
-                // err.innerText = '';
-            } else {
-                trow[i].style.display = "none";
-                // err.innerText = "Not found";
-            }
-        }
-        if (find) {
-            // msg.style.display = "none";
-            msg.classList.add("d-none");
-        } else {
-            // msg.style.display = "";
-            msg.classList.remove("d-none");
-        }
+    if( inputsearch.value !== '' ) {
+        filter = user.filter((item) => {
+            return item.username.toLowerCase() === inputsearch.value.toLowerCase();
+        });
+    } else {
+        filter = user;
     }
+    createtbl(filter);
 }
-
-// inputdata.addEventListener("keyup", function (e) {
-//     search(e);
-// });
 
 function myPopup() {
     let popUp = document.querySelector("#pop");
     popUp.classList.toggle("d-none");
 }
 
+// const user = [];
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then((data) => {
+//         user = data;
+//         createtbl(user);
+//     }); 
+//  console.log(user);
 
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(e => e = user);
-//  console.log(e)
+let user = [];
+async function userlist() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    user = await response.json();
+    createtbl(user);
+}
+userlist();
