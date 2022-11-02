ab3c18c9dd7963c6ff4011e65a928d7eaefebd86
Samkeet3377 <samkeet3377@gmail.com>
2022-11-02 22:31:52 +0530

    home

--- Angular/tryOuts/1Test/src/app/users/components/register/register.component.ts
+++ Angular/tryOuts/1Test/src/app/users/components/register/register.component.ts
@@ -22,8 +22,8 @@ export class RegisterComponent implements OnInit {
     this.signupForm = formBuilder.group({
       firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.minLength(3), Validators.maxLength(25)]],
       lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.minLength(3), Validators.maxLength(25)]],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9_]+@[a-z]+\.[a-z]{2,3}$'), Validators.minLength(8), Validators.maxLength(25)]],
+      password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9@$!_&]+$'), Validators.minLength(8), Validators.maxLength(30)]],
       phoneNumber: ['', [Validators.required]],
       address: formBuilder.group({
         state: ['', [Validators.required]],
