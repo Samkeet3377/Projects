fb42d6945e01590396abee8f2a583803af4b02a8
Samkeet Kevat <samkeet3377@gmail.com>
2022-11-03 18:01:54 +0530

    03-11-22

--- Angular/tryOuts/1Test/src/app/users/components/register/register.component.ts
+++ Angular/tryOuts/1Test/src/app/users/components/register/register.component.ts
@@ -23,8 +23,8 @@ export class RegisterComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.minLength(3), Validators.maxLength(25)]],
       lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.minLength(3), Validators.maxLength(25)]],
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9_]+@[a-z]+\.[a-z]{2,3}$'), Validators.minLength(8), Validators.maxLength(25)]],
-      password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9@$!_&]+$'), Validators.minLength(8), Validators.maxLength(30)]],
-      phoneNumber: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[$@!_&]).{8,30}$'), Validators.minLength(8), Validators.maxLength(30)]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^(?=.*?[0-9]).{10,10}$'), Validators.minLength(10), Validators.maxLength(10)]],
       address: formBuilder.group({
         state: ['', [Validators.required]],
         city: ['', [Validators.required]],
