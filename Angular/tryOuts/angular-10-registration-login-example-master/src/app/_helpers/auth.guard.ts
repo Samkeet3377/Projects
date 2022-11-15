0401bafa0cc8e567872aa2cf73ad14e96a35d501
Samkeet Kevat <samkeet3377@gmail.com>
2022-11-15 19:47:47 +0530

    15-11-22

--- Angular/tryOuts/angular-10-registration-login-example-master/src/app/_helpers/auth.guard.ts
+++ Angular/tryOuts/angular-10-registration-login-example-master/src/app/_helpers/auth.guard.ts
@@ -5,20 +5,21 @@ import { AccountService } from '@app/_services';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    constructor(
-        private router: Router,
-        private accountService: AccountService
-    ) {}
+  constructor(
+    private router: Router,
+    private accountService: AccountService
+  ) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const user = this.accountService.userValue;
-        if (user) {
-            // authorised so return true
-            return true;
-        }
-
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = this.accountService.userValue;
+    if (user) {
+      // authorised so return true
+      return true;
     }
+
+    // not logged in so redirect to login page with the return url
+    // this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+    this.router.navigate(['/account/login', { relativeTo: route.parent }]);
+    return false;
+  }
 }
